73633426715199eb21ce00fac6f855171669a8ff
Vinod Jatav <jatavvinod1997@gmail.com>
2023-01-30 22:43:11 +0530

    maximum sum subarray of size k

--- src/PatternSlidingWindow/MaximumSumSubarrayOfSizeK/MaximumSumSubarrayOfSizeK.java
+++ src/PatternSlidingWindow/MaximumSumSubarrayOfSizeK/MaximumSumSubarrayOfSizeK.java
@@ -3,7 +3,7 @@ package PatternSlidingWindow.MaximumSumSubarrayOfSizeK;
 public class MaximumSumSubarrayOfSizeK {
 
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
+		System.out.println("Maximum sum subarray of size k");
 
 	}
 

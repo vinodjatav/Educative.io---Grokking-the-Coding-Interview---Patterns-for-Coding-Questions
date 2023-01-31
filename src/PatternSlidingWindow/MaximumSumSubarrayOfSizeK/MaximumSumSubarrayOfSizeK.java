ed2a26f2b3bfa400cc774281937b0ecb79f60fda
Vinod Jatav <jatavvinod1997@gmail.com>
2023-01-31 22:46:20 +0530

    removing the comments

--- src/PatternSlidingWindow/MaximumSumSubarrayOfSizeK/MaximumSumSubarrayOfSizeK.java
+++ src/PatternSlidingWindow/MaximumSumSubarrayOfSizeK/MaximumSumSubarrayOfSizeK.java
@@ -3,7 +3,7 @@ package PatternSlidingWindow.MaximumSumSubarrayOfSizeK;
 public class MaximumSumSubarrayOfSizeK {
 
 	public static void main(String[] args) {
-		System.out.println("Maximum sum subarray of size k");
+		System.out.println("Maximum sum subarray of size k solution");
 
 	}
 

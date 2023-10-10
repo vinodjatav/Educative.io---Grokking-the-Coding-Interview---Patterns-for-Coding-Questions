0a03783d05792e907b4179fb12f12fdbd4cb5c67
Vinod Jatav <jatavvinod1997@gmail.com>
2023-10-11 00:20:49 +0530

    Update Average sum of subarraysof size K

--- src/PatternSlidingWindow/Introduction/AverageOfSubarraysOfSizeK.java
+++ src/PatternSlidingWindow/Introduction/AverageOfSubarraysOfSizeK.java
@@ -3,7 +3,7 @@ package PatternSlidingWindow.Introduction;
 import java.util.Arrays;
 
 public class AverageOfSubarraysOfSizeK {
-
+	// Brute force approach => Time complexity O(N*K)
 	public static double[] findAverages(int k, int[] arr) {
 		double[] result = new double[arr.length - k + 1];
 		for (int i = 0; i <= arr.length - k; i++) {
@@ -15,10 +15,32 @@ public class AverageOfSubarraysOfSizeK {
 		}
 		return result;
 	}
+	// The efficient way to solve this problem using sliding window => Time
+	// complexity O(N)
+	public static double[] findAveragesOptimized(int k, int[] arr) {
+		double[] result = new double[arr.length - k + 1];
+		double windowSum = 0;
+		int windowStart = 0;
+		for (int windowEnd = 0; windowEnd <= arr.length - 1; windowEnd++) {
+			windowSum += arr[windowEnd];
+			if (windowEnd >= k - 1) {
+				result[windowStart] = windowSum / k;
+				windowSum -= arr[windowStart];
+				windowStart++;
+			}
+		}
+		return result;
+	}
 
 	public static void main(String[] args) {
-		double[] result = AverageOfSubarraysOfSizeK.findAverages(5, new int[] { 1, 3, 2, 6, -1, 4, 1, 8, 2 });
-		System.out.println("Averages of subarrays of size K: " + Arrays.toString(result));
+		double[] bruteForceResult = AverageOfSubarraysOfSizeK.findAverages(5, new int[] { 1, 3, 2, 6, -1, 4, 1, 8, 2 });
+		System.out.println("Using brute force: ");
+		System.out.println("Averages of subarrays of size K: " + Arrays.toString(bruteForceResult));
+
+		double[] slidingwindowResult1 = AverageOfSubarraysOfSizeK.findAveragesOptimized(5,
+				new int[] { 1, 3, 2, 6, -1, 4, 1, 8, 2 });
+		System.out.println("Using sliding window: ");
+		System.out.println("Averages of subarrays of size K: " + Arrays.toString(slidingwindowResult1));
 	}
 
 }
